c40189cb1e5321e5a0868dfb990fc79b084475d4
r4don <compuradon@gmail.com>
2020-11-20 12:49:19 -0500

    added category field to auctionlisting

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -12,6 +12,7 @@ class AuctionListing(models.Model):
     photo = models.URLField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     date = models.DateTimeField(auto_now_add=True, null=True)
+    category = models.CharField(max_length=20, blank=True)
 
 class Bid(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
