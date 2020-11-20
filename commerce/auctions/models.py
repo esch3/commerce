8044e7f28ebf4f74487674d36136c4aff43290b4
r4don <compuradon@gmail.com>
2020-11-20 12:45:14 -0500

    altered photo field to url rather than image

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,7 +9,7 @@ class AuctionListing(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     title = models.CharField(max_length=50, blank=True)
     description = models.TextField(max_length=200, blank=True)
-    photo = models.ImageField(upload_to='auctions/listings')
+    photo = models.URLField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     date = models.DateTimeField(auto_now_add=True, null=True)
 
